1daeace7c4871a2f5f13ada9748b8ecefb6a1ec3
jamison0902 <jamison.queiroz@gmail.com>
2024-01-15 17:06:57 -0300

    Pratical_Mission2

--- practical-mission/CommonStyles.js
+++ practical-mission/CommonStyles.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: '#fff', marginTop: 10, width: '100%' }, scrollContainer: { width: '90%' },
+    container: { flex: 1, backgroundColor: '#fff', marginTop: 10, width: '100%',justifyContent: 'center', alignItems: 'center' }, scrollContainer: { width: '90%' },
 
     itemsContainer: { marginTop: 10, padding: 20, borderTopLeftRadius: 10, borderTopRightRadius: 10, alignItems: 'stretch', backgroundColor: '#fff' },
 
@@ -9,7 +9,7 @@ export const styles = StyleSheet.create({
 
     buttonsContainer: { flexDirection: 'row-reverse', alignItems: 'flex-end', borderBottomWidth: 1, borderBottomColor: '#CCC', paddingBottom: 10, marginTop: 10 },
 
-    input: { marginTop: 10, height: 60, backgroundColor: '#fff', borderRadius: 10, paddingHorizontal: 24, fontSize: 16, alignItems: 'stretch' },
+    input: { marginTop: 10, height: 60, backgroundColor: '#fff', borderRadius: 10, paddingHorizontal: 24, fontSize: 14, alignItems: 'stretch' },
 
     button: { marginTop: 10, marginBottom: 10, height: 60, backgroundColor: 'blue', borderRadius: 10, paddingHorizontal: 24, fontSize: 16, alignItems: 'center', justifyContent: 'center', elevation: 20, shadowOpacity: 20, shadowColor: '#ccc' },
 
@@ -17,7 +17,7 @@ export const styles = StyleSheet.create({
 
     buttonTextBig: { color: '#fff', fontWeight: 'bold', fontSize: 24 },
 
-    textItem: { fontSize: 20 },
+    textItem: { fontSize: 16 },
 
     deleteButton: { marginLeft: 10, height: 40, width: 40, backgroundColor: 'red', borderRadius: 10, padding: 10, fontSize: 12, elevation: 10, shadowOpacity: 10, shadowColor: '#ccc', alignItems: 'center' },
 
--- practical-mission/FornecedorItem.js
+++ practical-mission/FornecedorItem.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { styles } from './CommonStyles';
 
 export default function FornecedorItem(props) {
     return (
         <View style={styles.container} id={props.Fornecedor.codigo.toString()}>
+
+            <Image
+                style={{
+                    width: 60,
+                    height: 60,
+                    marginBottom: 15,
+                }}
+                source={require('./assets/alimento.png')}
+            />
+
             <Text style={styles.textItem}>
-                {props.Fornecedor.codigo} - {props.Fornecedor.nome}</Text>
+               Código: {props.Fornecedor.codigo}</Text>
+
+            <Text style={styles.textItem}>
+                Fornecedor: {props.Fornecedor.nome}</Text>
 
             <Text style={styles.textItem}>
                 Endereço: {props.Fornecedor.endereco}</Text>
